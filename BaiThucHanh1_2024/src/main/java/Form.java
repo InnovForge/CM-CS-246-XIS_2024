6f365e4f2deb42401cf545473c58b14e21d7965c
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-02-29 21:27:40 +0700

    change

--- BaiThucHanh1_2024/src/main/java/Form.java
+++ BaiThucHanh1_2024/src/main/java/Form.java
@@ -10,15 +10,15 @@ import java.awt.Font;
  *
  * @author bluez
  */
-public class Form_Sum2Numbers extends javax.swing.JFrame {
+public class Form extends javax.swing.JFrame {
 
     /**
      * Creates new form Form_Sum2Numbers
      */
-    public Form_Sum2Numbers() {
+    public Form() {
         initComponents();
         this.jLabelPHEPCONG.setFont(new Font("Arial",Font.PLAIN,30));
-        this.setTitle("Phép Cộng");
+        this.setTitle("Calculator");
     }
 
     /**
@@ -161,7 +161,7 @@ public class Form_Sum2Numbers extends javax.swing.JFrame {
             minusOf2Numbers minus = new minusOf2Numbers(Integer.parseInt(this.jTextFieldSoThu1.getText()), Integer.parseInt(this.jTextFieldSoThu2.getText()));
             this.jLabelKQMAIN.setText(minus.minus()+"");;
         } catch (NumberFormatException e) {
-            this.jLabelKQMAIN.setText("nhap du lieu sai s");
+            this.jLabelKQMAIN.setText("Nhap du lieu sai ");
         }
     }//GEN-LAST:event_jButtonTruActionPerformed
 
@@ -182,20 +182,21 @@ public class Form_Sum2Numbers extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Form_Sum2Numbers.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Form_Sum2Numbers.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Form_Sum2Numbers.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Form_Sum2Numbers.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Form_Sum2Numbers().setVisible(true);
+                new Form().setVisible(true);
             }
         });
     }
