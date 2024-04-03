672205947e4b395b6374760597215a8df567dee2
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-04-03 21:22:04 +0700

    add class ProductManagemnet

---
+++ ProductManagement/src/main/java/ProductManagement.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bluez
+ */
+public class ProductManagement {
+    private ArrayList<Product> products = new ArrayList<>();
+    public void addProduct(Product product)
+    {
+        products.add(product);
+    }
+    public void modifyProduct()
+    {
+        
+    }
+    public void deleteProduct()
+    {
+        
+    }
+    public void searchProduct()
+    {
+        
+    }
+    //them cac pham vao cuc bo
+    public void saveOnLocalStoge()
+    {
+        
+    }
+}
