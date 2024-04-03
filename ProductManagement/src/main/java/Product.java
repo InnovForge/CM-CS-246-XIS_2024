1c0fda21d4ade4b20e734f53b7a57eb73016f1a3
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-04-03 21:08:59 +0700

    add class Product

---
+++ ProductManagement/src/main/java/Product.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bluez
+ */
+public class Product {
+    private String productId;
+    private String productName;
+    private String productPrice;
+
+    public Product(String productId, String productName, String productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+     @Override
+    public String toString() {
+        return "Product{" + "productId=" + productId + ", productName=" + productName + ", productPrice=" + productPrice + '}';
+    }
+    
+}
