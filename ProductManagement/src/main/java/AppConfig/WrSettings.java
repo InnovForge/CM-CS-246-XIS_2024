d1b2dc27786ce94f77f47b2c396eea7740799840
ngtuonghy <ngtuonghy@gmail.com>
2024-04-04 22:09:24 +0700

    add appConfig

---
+++ ProductManagement/src/main/java/AppConfig/WrSettings.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AppConfig;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author harvous
+ */
+public class WrSettings {
+    private static final String _CONFIG_FILE = "Settings.properties";
+    private static Properties properties;
+    static {
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream(_CONFIG_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static void setProperty(String key, String value) {
+        properties.setProperty(key, value);
+        try {
+            properties.store(new FileOutputStream(_CONFIG_FILE), null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
